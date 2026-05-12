2df45bec81212b46f8dcd3db5eeb83ea7cd9dcc5
rajeshpadhy <rpadhy98@gmail.com>
2026-05-12 10:20:34 +0530

    12th May problem

---
+++ romToInteger.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let total =0;
+    let val ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+
+    for(let i =0;i<s.length;i++){
+        let char = s[i];
+        let curr = val[char]
+
+        let next = val[s[i+1]]||0;
+
+        if(curr<next){
+            total-=curr;
+        }else{
+            total+=curr;
+        }
+    }
+    return total;
+    
+}
